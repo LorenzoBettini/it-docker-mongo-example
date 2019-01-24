7511ca1aff4c88dee743dfe006ab2784e43995af
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-24 14:24:02 +0100

    created StudentMongoRepository

---
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -0,0 +1,34 @@
+package com.examples.school.repository.mongo;
+
+import java.util.List;
+
+import com.examples.school.model.Student;
+import com.examples.school.repository.StudentRepository;
+
+public class StudentMongoRepository implements StudentRepository {
+
+	@Override
+	public List<Student> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Student student) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
