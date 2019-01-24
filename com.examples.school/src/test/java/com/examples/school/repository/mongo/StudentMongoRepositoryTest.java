09f405c4b15a04ae32b934ccf678fc78326992a0
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-24 15:04:48 +0100

    created StudentMongoRepositoryTest

---
+++ com.examples.school/src/test/java/com/examples/school/repository/mongo/StudentMongoRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.examples.school.repository.mongo;
+
+import static com.examples.school.repository.mongo.StudentMongoRepository.SCHOOL_DB_NAME;
+import static com.examples.school.repository.mongo.StudentMongoRepository.STUDENT_COLLECTION_NAME;
+import static org.junit.Assert.*;
+
+import java.net.InetSocketAddress;
+
+import org.bson.Document;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+
+public class StudentMongoRepositoryTest {
+
+	private static MongoServer server;
+	private static InetSocketAddress serverAddress;
+
+	private MongoClient client;
+	private StudentMongoRepository studentRepository;
+	private MongoCollection<Document> studentCollection;
+
+	@BeforeClass
+	public static void setupServer() {
+		server = new MongoServer(new MemoryBackend());
+		// bind on a random local port
+		serverAddress = server.bind();
+	}
+
+	@AfterClass
+	public static void shutdownServer() {
+		server.shutdown();
+	}
+
+	@Before
+	public void setup() {
+		client = new MongoClient(new ServerAddress(serverAddress));
+		studentRepository = new StudentMongoRepository(client);
+		MongoDatabase database = client.getDatabase(SCHOOL_DB_NAME);
+		// make sure we always start with a clean database
+		database.drop();
+		studentCollection = database.getCollection(STUDENT_COLLECTION_NAME);
+	}
+
+	@After
+	public void tearDown() {
+		client.close();
+	}
+
+}
