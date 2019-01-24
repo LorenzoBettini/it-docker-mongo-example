19c95f4ca42e1e27ed55c497c1d4be1317db6b3e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-24 14:54:11 +0100

    constructor accepting MongoClient

--- com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -2,10 +2,24 @@ package com.examples.school.repository.mongo;
 
 import java.util.List;
 
+import org.bson.Document;
+
 import com.examples.school.model.Student;
 import com.examples.school.repository.StudentRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 
 public class StudentMongoRepository implements StudentRepository {
+
+	public static final String STUDENT_COLLECTION_NAME = "student";
+	public static final String SCHOOL_DB_NAME = "school";
+	private MongoCollection<Document> studentCollection;
+
+	public StudentMongoRepository(MongoClient client) {
+		studentCollection = client
+			.getDatabase(SCHOOL_DB_NAME)
+			.getCollection(STUDENT_COLLECTION_NAME);
+	}
 
 	@Override
 	public List<Student> findAll() {
