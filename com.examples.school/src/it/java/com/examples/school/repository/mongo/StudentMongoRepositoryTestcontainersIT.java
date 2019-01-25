0aec8108c0a089c812fb46cc2f9319fcefbd726b
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-25 10:06:18 +0100

    some integration tests for the repository

--- com.examples.school/src/it/java/com/examples/school/repository/mongo/StudentMongoRepositoryTestcontainersIT.java
+++ com.examples.school/src/it/java/com/examples/school/repository/mongo/StudentMongoRepositoryTestcontainersIT.java
@@ -3,6 +3,12 @@ package com.examples.school.repository.mongo;
 import static com.examples.school.repository.mongo.StudentMongoRepository.SCHOOL_DB_NAME;
 import static com.examples.school.repository.mongo.StudentMongoRepository.STUDENT_COLLECTION_NAME;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.bson.Document;
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +16,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 
+import com.examples.school.model.Student;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
@@ -46,7 +53,50 @@ public class StudentMongoRepositoryTestcontainersIT {
 	}
 
 	@Test
-	public void test() {
-		// just to check that we can connect to the container
+	public void testFindAll() {
+		addTestStudentToDatabase("1", "test1");
+		addTestStudentToDatabase("2", "test2");
+		assertThat(studentRepository.findAll())
+			.containsExactly(
+				new Student("1", "test1"),
+				new Student("2", "test2"));
+	}
+
+	@Test
+	public void testFindById() {
+		addTestStudentToDatabase("1", "test1");
+		addTestStudentToDatabase("2", "test2");
+		assertThat(studentRepository.findById("2"))
+			.isEqualTo(new Student("2", "test2"));
+	}
+
+	@Test
+	public void testSave() {
+		Student student = new Student("1", "added student");
+		studentRepository.save(student);
+		assertThat(readAllStudentsFromDatabase())
+			.containsExactly(student);
+	}
+
+	@Test
+	public void testDelete() {
+		addTestStudentToDatabase("1", "test1");
+		studentRepository.delete("1");
+		assertThat(readAllStudentsFromDatabase())
+			.isEmpty();
+	}
+
+	private void addTestStudentToDatabase(String id, String name) {
+		studentCollection.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
+	}
+
+	private List<Student> readAllStudentsFromDatabase() {
+		return StreamSupport.
+			stream(studentCollection.find().spliterator(), false)
+				.map(d -> new Student(""+d.get("id"), ""+d.get("name")))
+				.collect(Collectors.toList());
 	}
 }
